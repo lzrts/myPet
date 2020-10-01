2db1e6f9250e36b6d9722a25fa224871d9f1cf36
Andrew Lzrts <lazarets89@gmail.com>
2020-10-01 18:23:44 +0300

    fucker commit

--- src/test/java/smokeTest/instaMessage.java
+++ src/test/java/smokeTest/instaMessage.java
@@ -2,37 +2,59 @@ package smokeTest;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
-import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.$x;
 
 public class instaMessage {
 
-    String victimName = "top_lviv_boys";
+    private void login() {
+        $x("//input[@name='username']").sendKeys("ketapex165");
+        $x("//input[@name='password']").sendKeys("testonly1");
+        $x("//div[contains(text(),'Увійти')]").click();
+    }
+
     @Test
     void instaFuck() throws InterruptedException {
         Selenide.open("https://www.instagram.com/");
 
-        $x("//input[@name='username']").sendKeys("csdvdfv343");
-        $x("//input[@name='password']").sendKeys("testonly1");
-        $x("//div[contains(text(),'Увійти')]").click();
+        login();
 
         $x("//span[contains(text(),'Пошук')]").waitUntil(Condition.visible, 10000);
 
         Selenide.open("https://www.instagram.com/tosha.bandit/");
 
-      //  Thread.sleep(3000);
 
         $x("//article/div/div/div[1]/div[1]").waitUntil(Condition.visible, 10000).click();
 
 
-        for (int i=0; 1 <= 10; i++){
-            $x("//textarea").waitUntil(Condition.visible, 10000).sendKeys("ПРОДАВЕЦ ЧОРТ. КИДАЕТ НА БАБКИ!");
+        for (int i = 0; i <= 20; i++) {
+            $x("//textarea").waitUntil(Condition.visible, 5000).sendKeys("КИДАЕТ НА БАБКИ! ПРОДАВЕЦ ЧОРТ. КИДАЕТ НА БАБКИ! ПРОДАВЕЦ ЧОРТ.");
             $x("//button[@type='submit']").click();
-            Thread.sleep(10000);
+            Thread.sleep(5000);
             $x("//div/a[contains(text(),'Далі')]").click();
-            Thread.sleep(10000);
+            Thread.sleep(5000);
         }
+    }
+
+    @Test
+    void fuck2() {
+
+        Selenide.open("https://www.instagram.com/");
+
+        login();
+
+        $x("//span[contains(text(),'Пошук')]").waitUntil(Condition.visible, 10000);
+
+        Selenide.open("https://www.instagram.com/kimkardashian/");
+
+        $x("//button/div/*[local-name()='svg']/*[local-name()='circle']").click();
+        $x("//button[contains(text(),'Поскаржитися на користувача')]").click();
+        $x("//div[contains(text(),'Це неприйнятно')]").click();
+        $x("//div[contains(text(),'запис')]").click();
+        $x("//div[contains(text(),'публікує')]").click();
+        $x("//div[contains(text(),'Шахрайство')]").click();
+
+        System.out.println("Finish");
     }
 }
